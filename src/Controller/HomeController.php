8f13c1d59525dfcd4c5efce34555a2a0e3344d53
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-06-03 09:21:39 +0200

    Update HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,14 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
-class HomeController
+class HomeController extends AbstractController
 {
-    /**
-     * @var Environment
-     */
-    private $twig;
-
-    public function __construct(Environment $twig)
-    {
-        $this->twig = $twig;
-    }
-
     public function index(): Response
     {
-        return new Response($this->twig->render('home/index.html.twig'));
+        return $this->render('home/index.html.twig');
     }
 }
