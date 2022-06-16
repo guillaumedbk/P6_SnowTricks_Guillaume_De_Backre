374e0a44fa19587d663bd8011c75a32306cfd860
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-06-16 16:22:13 +0200

    hotfix

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TricksRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @var TricksRepository
+     */
+    private $tricksRepository;
+
+    public function index(TricksRepository $tricksRepository): Response
+    {
+        $this->tricksRepository = $tricksRepository;
+        $tricks = $this->tricksRepository->findAll();
+        return $this->render('home/index.html.twig',[
+            'tricks' => $tricks
+        ]);
+    }
+}
