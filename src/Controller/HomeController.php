a146d768d4e0e786089bc4d1e2962467dce4d7a7
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-06-20 11:57:59 +0200

    Conditional display of the connection status in the header

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\TricksRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authenticator\Token\PostAuthenticationToken;
 use Twig\Environment;
 
 class HomeController extends AbstractController
@@ -12,8 +13,11 @@ class HomeController extends AbstractController
     public function __invoke(TricksRepository $tricksRepository): Response
     {
         $tricks = $tricksRepository->findAll();
+        $user = $this->getUser();
+
         return $this->render('home/index.html.twig',[
-            'tricks' => $tricks
+            'tricks' => $tricks,
+            'user' => $user
         ]);
     }
 }
