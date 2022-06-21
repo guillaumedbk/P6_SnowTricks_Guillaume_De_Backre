7ca94647cdb5c3a6e3d16fb1b8d706fa591414d7
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-06-21 08:00:59 +0200

    Beginning of the Chat for a trick

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,18 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\ChatRepository;
 use App\Repository\TricksRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class TrickController extends AbstractController
 {
-    public function __invoke(TricksRepository $tricksRepository, int $id): Response
+    public function __invoke(TricksRepository $tricksRepository, int $id, ChatRepository $chatRepository, UserRepository $userRepository): Response
     {
         $trick = $tricksRepository->find($id);
+        $chats = $chatRepository->findBy(
+            ['trick' => $id]
+        );
+        $users = $userRepository->findAll();
 
         return $this->render('trick/trick.html.twig',[
             'trick' => $trick,
+            'chats' => $chats,
+            'users' => $users,
+            'id' => $id
         ]);
     }
 
