31522fc9a91ad71f86cd763414bc752f5e2f07d6
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-06-24 14:54:14 +0200

    Retrieve and set new password + success email message
    Recovery of the user according to his email. Use of the session.

--- src/Controller/ForgottenPasswordController.php
+++ src/Controller/ForgottenPasswordController.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +16,7 @@ use App\Form\ResetPasswordType;
 
 class ForgottenPasswordController extends AbstractController
 {
-    #[Route('/forgotten/password', name: 'app_update_password')]
+    #[Route('/forgotten/password', name: 'app_forgotten_password')]
     public function resetPassword(Request $request, UserRepository $userRepository, MailerInterface $mailer): Response
     {
         //RETRIEVE DATA IN THE DTO
@@ -26,6 +27,10 @@ class ForgottenPasswordController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $EmailDataDTO = $form->getData();
 
+            $user = $userRepository->findOneBy(array('email' => $EmailDataDTO->email));
+            $session = new Session();
+            $session->set('user', $user);
+
             //SEND URL TO THE USER
             $message =  (new TemplatedEmail())
                 ->from(new Address('debackre.guillaume@gmail.com', 'Guillaume - Snowtricks'))
@@ -33,10 +38,8 @@ class ForgottenPasswordController extends AbstractController
                 ->subject('Réinitialisation du mot de passe')
                 ->htmlTemplate('security/forgotten_password_email.html.twig');
 
-            // On envoie l'e-mail
+            //SEND EMAIL
             $mailer->send($message);
-
-
         }
 
         return $this->render('security/forgotten_password.html.twig', [
---
+++ src/Controller/UpdatePasswordController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\UpdatePasswordType;
+use App\Repository\UserRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UpdatePasswordController extends AbstractController
+{
+    #[Route('/update/password', name: 'app_update_password')]
+    public function updatePassword(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasher, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(UpdatePasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //RETRIEVE NEW PASSWORD
+            $passwordDTO = $form->getData();
+
+            //GET USER
+            $session = new Session();
+            $user = $session->get('user');
+
+            //SET HASHED PASSWORD
+            $user = $userRepository->find($user->getId());
+            $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $passwordDTO->password
+            ));
+
+            //SEND SUCCESS MESSAGE
+            $message =  (new TemplatedEmail())
+                ->from(new Address('debackre.guillaume@gmail.com', 'Guillaume - Snowtricks'))
+                ->to($user->getEmail())
+                ->subject('Mot de passe mis à jour avec succès')
+                ->htmlTemplate('security/success_update_password.html.twig');
+
+            //SEND EMAIL
+            $mailer->send($message);
+
+        }
+
+        return $this->render('security/update_password.html.twig', [
+            'UpdatePasswordForm' => $form->createView()
+        ]);
+    }
+}
