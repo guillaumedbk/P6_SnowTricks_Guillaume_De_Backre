5131cb33e389338ab5f37d7e215a182ec4c29ecf
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-06-30 17:48:12 +0200

    Placeholder + require for trick creation form

--- src/Form/CreateTrickType.php
+++ src/Form/CreateTrickType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\DTO\TrickDTO;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,10 +15,22 @@ class CreateTrickType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('imageUrl')
-            ->add('videoUrl')
+            ->add('title', TextType::class, [
+                'attr' => array('placeholder' => 'Trick title'),
+                'required' => true
+            ])
+            ->add('description', TextType::class, [
+                'attr' => array('placeholder' => 'Trick description'),
+                'required' => false
+            ])
+            ->add('imageUrl', UrlType::class, [
+                'attr' => array('placeholder' => 'ex: https://cdn.pixabay.com/snowboard.jpg'),
+                'required' => false
+            ])
+            ->add('videoUrl', UrlType::class, [
+                'attr' => array('placeholder' => 'ex: https://www.youtube.com/embed/'),
+                'required' => false
+            ])
             ->add('images', FileType::class, [
                 'multiple' => true,
                 'mapped' => false,
