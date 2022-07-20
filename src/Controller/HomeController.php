e2f2fe03b2a8173ff8ae5f661034a073b2b0f9ac
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-07-20 14:50:59 +0200

    Load More button

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\ImageRepository;
 use App\Repository\TricksRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +13,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route(path: '/', name: 'app_homepage')]
-    public function __invoke(TricksRepository $tricksRepository, ImageRepository $imageRepository): Response
+    public function __invoke(Request $request, TricksRepository $tricksRepository, ImageRepository $imageRepository): Response
     {
-        $tricks = $tricksRepository->findAll();
+        $trickLoad = (int)$request->query->get("trickLoad", 10);
+        $allTricks = $tricksRepository->findAll();
+        $total = count($allTricks);
+        $tricks = $tricksRepository->findBy([], [], $trickLoad);
         $user = $this->getUser();
 
         return $this->render('home/index.html.twig',[
+            'total' => $total,
+            'trickLoad' => $trickLoad,
             'tricks' => $tricks,
             'user' => $user,
         ]);
